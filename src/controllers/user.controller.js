2dce834f40943cbf8494864f5b117653d7a2f63f
Sunnyiitg <sunnyfromiitg@gmail.com>
2024-06-17 15:10:46 +0530

    correction

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -249,7 +249,7 @@ const getCurrentUser = asyncHandler(async(req,res) => {
     // because of middle ware req.user
     return res
             .status(200)
-            .json(200,req.user,"current user fetched");
+            .json(new ApiResponse(200,req.user,"user fetched"));
 })
 
 const updateAccountDetails = asyncHandler(async(req,res) => {
@@ -257,7 +257,7 @@ const updateAccountDetails = asyncHandler(async(req,res) => {
     if(!fullName && !email) {
         throw new ApiError(400,"All field are missing");
     }
-    const user = User.findByIdAndUpdate(
+    const user = await User.findByIdAndUpdate(
         req.user?._id,
         {
             $set : {
